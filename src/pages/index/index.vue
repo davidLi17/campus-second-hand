b731cfe9ade9b0ab24a63286ca3e4bffe3ac334b
lihaoge <lihaoge@bytedance.com>
2025-04-14 21:28:40 +0800

    feat:首页新增三个子组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-//
+import { CustomNavbar } from './componets/CustomNavbar.vue'
+import { CategoryPanel } from './componets/CategoryPanel.vue'
 </script>
 
 <template>
-  <view class="index">index</view>
+  <view class="index">
+    <view>
+      <CustomNavbar></CustomNavbar>
+      <XtxSwiper></XtxSwiper>
+      <CategoryPanel></CategoryPanel>
+    </view>
+  </view>
 </template>
 
 <style lang="scss">
